26c9847da757dc162978405a3b5effac8173116e
Login and Signup <raveeshanethmina@gmail.com>
2025-12-25 23:37:22 +0530

    feat: update Lenis package references; replace '@studio-freight/lenis' with 'lenis'

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           'gsap': ['gsap'],
-          'lenis': ['@studio-freight/lenis'],
+          'lenis': ['lenis'],
         },
       },
     },
